1dcaf180ba38c577b1e1bf09a4dffba4fcd7b8a7
Andy Sigler <andrewsigler1@gmail.com>
2023-12-12 16:40:19 -0500

    slow down sensors reading interval so help ot3controller

--- hardware/opentrons_hardware/scripts/sensor_utils.py
+++ hardware/opentrons_hardware/scripts/sensor_utils.py
@@ -131,7 +131,7 @@ async def handle_pressure_sensor(
             print(f"got no data: {curr_time}")
             await sleep(0.1)
             return await _read(_s)
-        await sleep(0.025)
+        await sleep(0.05)
         return t, d.to_float()
 
     while datetime.now() < end_time:
