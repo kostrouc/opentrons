62d16c24ebc9562551ca275f281fa226c69a31f3
Nicholas Shiland <nicholas.shiland@opentrons.com>
2024-08-08 14:25:03 -0400

    spacing

--- abr-testing/abr_testing/hw_test_scripts/gripper_and_z_test.py
+++ abr-testing/abr_testing/hw_test_scripts/gripper_and_z_test.py
@@ -13,7 +13,6 @@ from opentrons_shared_data.errors.exceptions import (
     StallOrCollisionDetectedError,
     PythonException,
 )
-
 from hardware_testing.opentrons_api.types import (
     OT3Mount,
     Axis,
@@ -25,33 +24,38 @@ from hardware_testing.opentrons_api.helpers_ot3 import (
 
 
 async def _main(
-    mount: OT3Mount, mount_name: str, simulate: bool, time_min: int, z_axis: Axis) -> None:
-    
-    #make directory for tests. check if directory exists, make if doesn't.
+    mount: OT3Mount, mount_name: str, simulate: bool, time_min: int, z_axis: Axis
+) -> None:
+
+    # make directory for tests. check if directory exists, make if doesn't.
     BASE_DIRECTORY = "/userfs/data/testing_data/gripper_and_z_test/"
     if not os.path.exists(BASE_DIRECTORY):
         os.makedirs(BASE_DIRECTORY)
 
-    #set limits then grab input distance.
+    # set limits then grab input distance.
     limit = 150
     if mount_name != "gripper":
         limit = 210
     while True:
         try:
-            distance = float(input(f"How far would you like the z axis to travel? The range is between 1 and {str(limit)}: "))
+            distance = float(
+                input(
+                    f"How far would you like the z axis to travel? The range is between 1 and {str(limit)}: "
+                )
+            )
             if 0 < int(distance) <= limit:
                 break
             else:
                 print(f"Please choose a value between 1 and {str(limit)}.")
         except ValueError:
             print(f"Please enter a number.")
-    
-    #Ask, get, and test Jira ticket link
+
+    # Ask, get, and test Jira ticket link
     want_comment = False
     while True:
         y_or_no = input("Do you want to attach the results to a JIRA Ticket? Y/N: ")
         if y_or_no == "Y" or y_or_no == "y":
-            #grab testing teams jira api info from a local file
+            # grab testing teams jira api info from a local file
             storage_directory = "/var/lib/jupyter/notebooks"
             jira_info = os.path.join(storage_directory, "jira_credentials.json")
             # create an dict copying the contents of the testing team jira info
@@ -66,20 +70,13 @@ async def _main(
                     f"Please add json file with the testing team jira credentials to: {storage_directory}."
                 )
             want_comment = True
-            while True:    
+            while True:
                 issue_key = input("Ticket Key: ")
                 url = f"https://opentrons.atlassian.net/rest/api/3/issue/{issue_key}"
                 auth = HTTPBasicAuth(email, api_token)
 
-                headers = {
-                "Accept": "application/json"
-                }
-                response = requests.request(
-                "GET",
-                url,
-                headers=headers,
-                auth=auth
-                )
+                headers = {"Accept": "application/json"}
+                response = requests.request("GET", url, headers=headers, auth=auth)
                 if str(response) == "<Response [200]>":
                     break
                 else:
@@ -91,7 +88,7 @@ async def _main(
         else:
             print("Please Choose a Valid Option")
 
-    #get and confirm robot IP address
+    # get and confirm robot IP address
     while True:
         ip = input("Robot IP: ")
         # From health: robot name
@@ -99,7 +96,7 @@ async def _main(
             response = requests.get(
                 f"http://{ip}:31950/health", headers={"opentrons-version": "3"}
             )
-            #confirm connection of IP
+            # confirm connection of IP
             if str(response) == "<Response [200]>":
                 break
             else:
@@ -118,7 +115,7 @@ async def _main(
         for item in instruments["data"]:
             if item["mount"] == "extension":
                 instrument_serial = item["serialNumber"]
-        
+
     else:
         response = requests.get(
             f"http://{ip}:31950/pipettes", headers={"opentrons-version": "3"}
@@ -136,7 +133,12 @@ async def _main(
     time_start = current_datetime.strftime("%m-%d, at %H-%M-%S")
 
     init_data = [
-        [f"Robot: {robot_name}", f" Mount: {mount_name}", f" Distance: {distance}", f" Instrument Serial: {instrument_serial}"],
+        [
+            f"Robot: {robot_name}",
+            f" Mount: {mount_name}",
+            f" Distance: {distance}",
+            f" Instrument Serial: {instrument_serial}",
+        ],
     ]
 
     file_path = f"{BASE_DIRECTORY}/{robot_name} test on {time_start}"
@@ -147,7 +149,7 @@ async def _main(
 
     input("press enter to continue")
 
-    #hw api setup
+    # hw api setup
     hw_api = await build_async_ot3_hardware_api(
         is_simulating=simulate, use_defaults=True
     )
@@ -157,8 +159,8 @@ async def _main(
     timeout = time_min * 60
     count = 0
     errored = False
-    #finding home and starting to move
-    
+    # finding home and starting to move
+
     try:
         await hw_api.home()
         await asyncio.sleep(1)
@@ -170,7 +172,7 @@ async def _main(
             # grab and print time and move count
             count += 1
             print(f"cycle: {count}")
-            runtime = time.time()-timeout_start
+            runtime = time.time() - timeout_start
             print(f"time: {runtime}")
             # write count and runtime to csv sheet
             run_data = [
@@ -192,71 +194,55 @@ async def _main(
         print(f"THIS ERROR WAS: {errorn}")
         error_message = str(errorn)
     finally:
-        #Grab info and comment on JIRA
+        # Grab info and comment on JIRA
         await hw_api.disengage_axes([Axis.X, Axis.Y, Axis.Z, Axis.G])
         await hw_api.clean_up()
         if want_comment == True:
-            with open(file_path, newline='') as csvfile:
-                csvobj = csv.reader(csvfile, delimiter=',')
-                
+            with open(file_path, newline="") as csvfile:
+                csvobj = csv.reader(csvfile, delimiter=",")
+
                 full_list = list(csvobj)
                 row_of_interest = full_list[count]
                 cropped_cycle = str(row_of_interest).split("'")[1]
                 cropped_time = str(row_of_interest).split("'")[3]
                 cropped_time = cropped_time[1:]
-            
+
             if errored == True:
                 comment_message = f"This test failed due to {error_message} on {cropped_cycle} and {cropped_time}."
             else:
                 comment_message = f"This test successfully completed at {cropped_cycle} and {cropped_time}."
 
-            #use REST to comment on JIRA ticket
+            # use REST to comment on JIRA ticket
             comment_url = url + "/comment"
-            headers = {
-            "Accept": "application/json",
-            "Content-Type": "application/json"
-            }
-            payload = json.dumps( {
-            "body": {
-                "content": [
+            headers = {"Accept": "application/json", "Content-Type": "application/json"}
+            payload = json.dumps(
                 {
-                    "content": [
-                    {
-                        "text": comment_message,
-                        "type": "text"
-                    }
-                    ],
-                    "type": "paragraph"
+                    "body": {
+                        "content": [
+                            {
+                                "content": [{"text": comment_message, "type": "text"}],
+                                "type": "paragraph",
+                            }
+                        ],
+                        "type": "doc",
+                        "version": 1,
+                    },
                 }
-                ],
-                "type": "doc",
-                "version": 1
-            },
-            } )
-            response = requests.request(
-            "POST",
-            comment_url,
-            data=payload,
-            headers=headers,
-            auth=auth
             )
-            #post csv file created to jira ticket
+            response = requests.request(
+                "POST", comment_url, data=payload, headers=headers, auth=auth
+            )
+            # post csv file created to jira ticket
             attachment_url = url + "/attachments"
-            headers = {
-                "Accept": "application/json",
-                "X-Atlassian-Token": "no-check"
-            }
+            headers = {"Accept": "application/json", "X-Atlassian-Token": "no-check"}
 
             response = requests.request(
-            "POST",
-            attachment_url,
-            headers=headers,
-            auth=auth,
-            files = {
-                "file": (file_path, open(file_path,"rb"), "application-type")
-            }
+                "POST",
+                attachment_url,
+                headers=headers,
+                auth=auth,
+                files={"file": (file_path, open(file_path, "rb"), "application-type")},
             )
-                
 
 
 def main() -> None:
